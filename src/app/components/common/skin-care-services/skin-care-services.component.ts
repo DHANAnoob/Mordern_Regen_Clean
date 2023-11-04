5ee6a6a38d5f5376e593a328c5638880f02ebec5
Dhanasekar <jdhanasekar1999@gmail.com>
2023-11-04 20:10:58 +0530

    content change

--- src/app/components/common/skin-care-services/skin-care-services.component.ts
+++ src/app/components/common/skin-care-services/skin-care-services.component.ts
@@ -9,16 +9,34 @@ export class SkinCareServicesComponent implements OnInit {
 
   skin: boolean = true;
   ortho: boolean = false;
+  tissueRegen :boolean = false;
+  sexualHealth:boolean = false;
 
   constructor() { }
  
   trigger(){
     this.skin = true;
     this.ortho = false;
+    this.tissueRegen = false;
+    this.sexualHealth = false;
   }
   newTrigger(){
     this.skin = false;
     this.ortho = true;
+    this.tissueRegen = false;
+    this.sexualHealth = false;
+  }
+  newTrigger1(){
+    this.skin = false;
+    this.ortho = false;
+    this.tissueRegen = true;
+    this.sexualHealth = false;
+  }
+  newTrigger2(){
+    this.skin = false;
+    this.ortho = false;
+    this.tissueRegen = false;
+    this.sexualHealth = true;
   }
   ngOnInit(): void {
   }
